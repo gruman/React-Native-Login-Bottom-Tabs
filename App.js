6de5578a3d8bc3865cac6bf3e329f363f337db3d
Matthew Gruman <matthew.gruman@itdcanada.ca>
2023-09-04 16:12:08 -0700

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native';
+
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+import Home from './screens/Home';
+import Profile from './screens/Profile';
+import Search from './screens/Search';
+import Login from './screens/Login';
+
+import { NavigationContainer } from '@react-navigation/native';
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
+
+  const user = true;
+
+  function Tabs() {
+    return (
+
+      <Tab.Navigator initialRouteName="Home">
+
+      <Tab.Screen name="Search" component={Search} options={{
+          title: "Search",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="search-outline" color={color} size={size} />
+          ),
+        }} />
+        <Tab.Screen name="Home" component={Home} options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" color={color} size={size} />
+          )
+        }} />
+        <Tab.Screen name="Profile" component={Profile} options={{
+          title: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-outline" color={color} size={size} />
+          )
+        }} />
+      </Tab.Navigator>
+    )
+  }
+
+  function Stacks() {
+    return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} title="Login" />
+    </Stack.Navigator>
+    );
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <SafeAreaView style={{flex: 1 }}>
+          {
+            user ?
+          <Tabs />
+          :
+          <Stacks />
+          }
+        </SafeAreaView>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ README.md
@@ -0,0 +1,6 @@
+# React Native Bottom Tabs with Stack Login
+ 
+ This is a starter pack for making an app with bottom tabs hidden behind a login page. Change the user variable in App.js to true to see the inside pages.
+
+
+ https://matthewgruman.com
